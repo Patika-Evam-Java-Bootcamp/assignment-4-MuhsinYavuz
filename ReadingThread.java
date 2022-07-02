6ffea241c0a9be6902c2754fa7c0ab0cef9da7ec
Muhsin <80474514+MuhsinYavuz@users.noreply.github.com>
2022-07-02 09:42:16 +0300

    Update ReadingThread.java

--- ReadingThread.java
+++ ReadingThread.java
@@ -12,7 +12,7 @@ public class ReadingThread implements Runnable{
     public void run() {
         try {
             System.out.println("Json Dosyası okunuyor ");
-            JsonService jsonService = new JsonService();
+            JsonService jsonService = new JsonService(); // json ile okuma işlemi yapılarak veriler students listesine aktarılacak
             System.out.println("Okuma işlemi tamamlandı . ");
             ThreadQueue.isRead = true ;   // okuma işlemi tamamlandı şimdi yazmaya geçebilir .
         } catch (IOException e) {
